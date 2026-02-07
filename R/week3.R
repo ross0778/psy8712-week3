d7d60a729c68002bc34c3b1e88ed57eca2fe77c5
ross0778 <ross0778@umn.edu>
2026-02-07 13:32:56 -0600

    Create frequency tables list variable

--- R/week3.R
+++ R/week3.R
@@ -7,3 +7,8 @@ raw_df$timeStart<- as.POSIXct(raw_df$timeStart, format="%Y-%m-%d %H:%M:%S")
 raw_df$timeEnd<- as.POSIXct(raw_df$timeEnd, format="%Y-%m-%d %H:%M:%S")
 clean_df<- raw_df[raw_df$timeStart>=as.POSIXct("2017-07-01"),]
 clean_df<- clean_df[clean_df$q6==1,]
+
+# Analysis
+clean_df$timeSpent<- difftime(clean_df$timeEnd, clean_df$timeStart, units="secs")
+hist(as.numeric(clean_df$timeSpent))
+frequency_tables_list<- lapply(clean_df[5:14], table)
