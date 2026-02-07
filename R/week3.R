bf5d84ec995b321cda6d7751279d39225bd9d7cc
ross0778 <ross0778@umn.edu>
2026-02-07 09:22:02 -0600

    Create data frame and convert time formats

--- R/week3.R
+++ R/week3.R
@@ -1,2 +1,7 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Data Import and Cleaning
+raw_df<- read.csv(file="data/week3.csv", header=TRUE)
+raw_df$timeStart<- as.POSIXct(raw_df$timeStart, format="%Y-%m-%d %H:%M:%S")
+raw_df$timeEnd<- as.POSIXct(raw_df$timeEnd, format="%Y-%m-%d %H:%M:%S")
