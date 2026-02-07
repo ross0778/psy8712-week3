423ffea8cd6c046c62d8a8fb186aeee2982080a1
ross0778 <ross0778@umn.edu>
2026-02-07 09:32:06 -0600

    Finish cleaning clean_df

--- R/week3.R
+++ R/week3.R
@@ -5,3 +5,5 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 raw_df<- read.csv(file="data/week3.csv", header=TRUE)
 raw_df$timeStart<- as.POSIXct(raw_df$timeStart, format="%Y-%m-%d %H:%M:%S")
 raw_df$timeEnd<- as.POSIXct(raw_df$timeEnd, format="%Y-%m-%d %H:%M:%S")
+clean_df<- raw_df[raw_df$timeStart>=as.POSIXct("2017-07-01"),]
+clean_df<- clean_df[clean_df$q6==1,]
